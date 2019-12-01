d88e124fbef310738d792dc9f045bbbef590524a
Angel Salvador <angelsalvadordev@gmail.com>
2019-11-30 22:38:37 -0500

    agregado localstorage y mensajes de alerta

--- js/app.js
+++ js/app.js
@@ -3,52 +3,65 @@ new Vue({
   data: {
     nameApp: "ToDo VueJS",
     beforeTitleCache: '',
+    alertMessage: '',
+    alertClass: 'alert-success',
+    newTask: '',
     tasks: [
       {
-        title: "tarea 1",
+        title: "tarea de prueba",
         done: false,
         editing: false
       },
-      {
-        title: "tarea 2",
-        done: false,
-        editing: false
-      },
-      {
-        title: "tarea 3",
-        done: false,
-        editing: false
-      }
-
     ]
   },
   methods: {
+    showMessage: function ({ message, className }) {
+      this.alertMessage = message
+      this.alertClass = className
+      setTimeout(() => {
+        this.alertMessage = ''
+        this.alertClass = ''
+      }, 4000);
+    },
     addTask: function (e) {
-      if (this.tasks.title) {
-        if (this.tasks.title.length <= 5) {
-          alert('Se requiere minimo 6 caracteres para agregar una tarea')
-          return
-        }
-      } else {
-        alert('Por favor, ingresa el nombre de la tarea')
+      console.log(this.newTask)
+      if (this.newTask.length <= 5) {
+        this.showMessage({
+          message: 'Se requiere <strong>minimo 6 caracteres</strong> para agregar una tarea',
+          className: "alert-danger"
+        })
         return
       }
 
       this.tasks.push({
-        title: this.tasks.title,
-        done: false
+        title: this.newTask,
+        done: false,
+        editing: false
+      })
+      localStorage.setItem('todo-vue', JSON.stringify(this.tasks))
+      this.newTask = ''
+      this.showMessage({
+        message: 'Tarea agregada con exito!',
+        className: "alert-success"
       })
     },
     editTask: function (task) {
+      console.log('click')
       this.beforeTitleCache = task.title
       task.editing = true
     },
     doneEdit: function (task) {
       if (task.title.trim() === '') {
         task.title = this.beforeTitleCache
+        return
       }
 
+      localStorage.setItem('todo-vue', JSON.stringify(this.tasks))
       task.editing = false
+      this.showMessage({
+        message: 'Tarea modificada con exito!',
+        className: "alert-info"
+      })
     },
     cancelEdit: function (task) {
       task.title = this.beforeTitleCache
@@ -58,7 +71,15 @@ new Vue({
       if (confirm('Estas seguro de eliminar esta tarea?')) {
         var index = this.tasks.indexOf(task)
         this.tasks.splice(index, 1)
+        localStorage.setItem('todo-vue', JSON.stringify(this.tasks))
       }
     }
+  },
+  created: function () {
+    let dataLocal = JSON.parse(localStorage.getItem('todo-vue'))
+    if (dataLocal === null) return
+
+    this.tasks.splice(0, this.tasks.length)
+    this.tasks.push(...dataLocal)
   }
 });
